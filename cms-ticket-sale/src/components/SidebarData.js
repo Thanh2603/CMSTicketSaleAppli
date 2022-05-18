d54a6dd3837b0d3e29ed25c9e006fafa1259fa2b
Thanh2603 <86702148+Thanh2603@users.noreply.github.com>
2022-05-19 00:06:30 +0700

    Navigation

--- cms-ticket-sale/src/components/SidebarData.js
+++ cms-ticket-sale/src/components/SidebarData.js
@@ -1,11 +1,27 @@
 import React from 'react'
-
+import HomeIcon from '@mui/icons-material/Home';
+import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
+import LocalActivityIcon from '@mui/icons-material/LocalActivity';
+import SettingsIcon from '@mui/icons-material/Settings';
 export const SidebarData =[
     {
-        
-    }
+        title: "Trang Chủ",
+        icon: <HomeIcon />,
+        link: "/home"
+    },
+    {
+        title: "Quản Lý Vé",
+        icon: <ConfirmationNumberIcon />,
+        link: "/ticket"
+    },
+    {
+        title: "Đối Soát Vé",
+        icon: <LocalActivityIcon />,
+        link: "/checkingticket"
+    },
+    {
+        title: "Cài Đặt",
+        icon: <SettingsIcon />,
+        link: "/setting"
+    },
 ] 
- 
-
-
-export default SidebarData
