fda47101f657b522abacbdcbe3b2c28569b8f4bf
Thanh2603 <86702148+Thanh2603@users.noreply.github.com>
2022-05-22 02:09:24 +0700

    SearchIcon

--- cms-ticket-sale/src/components/Sidebar.js
+++ cms-ticket-sale/src/components/Sidebar.js
@@ -1,6 +1,9 @@
+import { faBell, faEnvelope, faMagnifyingGlass, faUserTie } from '@fortawesome/free-solid-svg-icons';
 import React from 'react'
 import "../App.css";
 import {SidebarData} from './SidebarData'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 function Sidebar() {
   return (
     <div className='Sidebar'>
@@ -16,14 +19,34 @@ function Sidebar() {
         onClick={()=>{window.location.pathname = val.link}}> <div id="icon"> {val.icon} </div>{" "}
         <div id="title">
           {val.title}
-        </div>
+        </div>      
+   
          </li>;
-      })}
+      })}     
       </ul>
-      <div className='searchfilter'>
-           <input type="text" value ="Search"/>
-           
-        </div>
+      <div className='search'>
+           <input placeholder='Search'/>
+           <button className='search-btn'><FontAwesomeIcon icon= {faMagnifyingGlass}/>
+           </button>
+      </div>
+      <div className='notimail'>
+        <button className='mail'>
+      <FontAwesomeIcon icon={faEnvelope}/>
+        </button>
+
+        <button className='bell'>
+        <FontAwesomeIcon icon={faBell}/>
+        </button>
+
+        <button className='accnotimail'>
+      <FontAwesomeIcon icon={faUserTie}/>
+        </button>
+      </div>
+      <div >
+        <p className='p1'>Copyright</p>
+        <p className='p2'>@</p>
+        <p className='p3'>2020 Alta Software</p>
+      </div>
     </div>
   )
 }
--- cms-ticket-sale/src/components/SidebarData.js
+++ cms-ticket-sale/src/components/SidebarData.js
@@ -3,7 +3,7 @@ import HomeIcon from '@mui/icons-material/Home';
 import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
 import LocalActivityIcon from '@mui/icons-material/LocalActivity';
 import SettingsIcon from '@mui/icons-material/Settings';
-import SearchIcon from '@mui/icons-material/Search';
+
 export const SidebarData =[
     {
         title: "Trang Chủ",
@@ -25,7 +25,4 @@ export const SidebarData =[
         icon: <SettingsIcon />,
         link: "/setting"
     },
-    {
-        
-    }
 ] 
